89b83ba6ecff3241c086ba84f8608de8fa55163c
Matyusha12 <nataliyayakovleva90@icloud.com>
2024-05-19 20:57:19 -0600

    commit

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,36 +1,33 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
 
-const invCont = {}
+const invCont = {};
 
-/* ***************************
- *  Build inventory by classification view
- * ************************** */
+// Build inventory by classification view
 invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId;
   const data = await invModel.getInventoryByClassificationId(classification_id);
+  if (!data || data.length === 0) {
+    return res.status(404).send('Classification not found');
+  }
   const grid = await utilities.buildClassificationGrid(data);
-  let nav = await utilities.getNav();
+  const nav = await utilities.getNav();
   const className = data[0].classification_name;
-  res.render("./inventory/classification", {
+  res.render("inventory/classification", {
     title: `${className} vehicles`,
     nav,
     grid,
   });
 };
 
-/* ***************************
- *  Display inventory item detail view
- * ************************** */
+// Display inventory item detail view
 invCont.displayInventoryDetail = async (req, res, next) => {
   try {
     const inventoryId = req.params.inventoryId;
     const vehicle = await invModel.getInventoryById(inventoryId);
     if (!vehicle) {
-      res.status(404).send('Vehicle not found');
-      return;
+      return res.status(404).send('Vehicle not found');
     }
-  
     res.render("inventory/detail", {
       title: `${vehicle.make} ${vehicle.model}`,
       vehicle,
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,16 +1,21 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const invController = require("../controllers/invController")
-
+const express = require("express");
+const router = express.Router();
+const invController = require("../controllers/invController");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
-router.get('/detail/:inventoryId', invController.displayInventoryDetail);
-router.get('/trigger-error', (req, res) => {
-    throw new Error('Test Error for 500 response');
-  });
+// Route to display inventory detail
+router.get("/detail/:inventoryId", invController.displayInventoryDetail);
+
+// Trigger a test error
+router.get("/trigger-error", (req, res, next) => {
+  try {
+    throw new Error("Test Error for 500 response");
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
 
