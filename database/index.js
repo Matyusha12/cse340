c5d251ffdceb412328f91949d70c3f17f552dc34
Matyusha12 <nataliyayakovleva90@icloud.com>
2024-06-04 17:48:40 -0600

    commit all

--- database/index.js
+++ database/index.js
@@ -16,25 +16,40 @@ if (process.env.NODE_ENV === "development") {
             rejectUnauthorized: false,
         }
     });
+
+    // Added for troubleshooting queries during development
+    module.exports = {
+        query: async (text, params) => {
+            try {
+                const res = await pool.query(text, params);
+                console.log("executed query", { text, res });
+                return res;
+            } catch (error) {
+                console.error("error in query", { text, error });
+                throw error;
+            }
+        },
+        pool
+    };
 } else {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: {
-            rejectUnauthorized: true,
+            rejectUnauthorized: false, 
         }
     });
-}
 
-module.exports = {
-    query: async (text, params) => {
-        try {
-            const res = await pool.query(text, params);
-            console.log("executed query", { text, res });
-            return res;
-        } catch (error) {
-            console.error("error in query", { text, error });
-            throw error;
-        }
-    },
-    pool
-};
+    module.exports = {
+        query: async (text, params) => {
+            try {
+                const res = await pool.query(text, params);
+                console.log("executed query", { text, res });
+                return res;
+            } catch (error) {
+                console.error("error in query", { text, error });
+                throw error;
+            }
+        },
+        pool
+    };
+}
